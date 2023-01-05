316be9b35dd6864e0c1fe6c6c71910571d977695
cullvox <cullvox@outlook.com>
2023-01-05 12:52:55 -0500

    Add more render device functions

--- Source/RenderDevice.cpp
+++ Source/RenderDevice.cpp
@@ -1,4 +1,8 @@
+#include "vulkan/vulkan_core.h"
 #include <stdexcept>
+
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
 
 #include "RenderDevice.h"
 
@@ -7,7 +11,7 @@ Buffer::Buffer(VmaAllocator allocator, VkDeviceSize size, VkBufferUsageFlags usa
     : mAllocator(allocator)
 {
 
-    VkBufferCreateInfo bufferCreateInfo = {0};
+    VkBufferCreateInfo bufferCreateInfo = {};
     bufferCreateInfo.sType                 = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
     bufferCreateInfo.pNext                 = nullptr;
     bufferCreateInfo.flags                 = 0;
@@ -16,7 +20,7 @@ Buffer::Buffer(VmaAllocator allocator, VkDeviceSize size, VkBufferUsageFlags usa
     bufferCreateInfo.queueFamilyIndexCount = 1;
     bufferCreateInfo.pQueueFamilyIndices   = &graphicsQueueFamilyIndex;
 
-    VmaAllocationCreateInfo allocationCreateInfo = {0};
+    VmaAllocationCreateInfo allocationCreateInfo = {};
     allocationCreateInfo.flags          = 0;
     allocationCreateInfo.usage          = memoryUsage;
     allocationCreateInfo.requiredFlags  = requiredFlags;
@@ -30,11 +34,97 @@ Buffer::Buffer(VmaAllocator allocator, VkDeviceSize size, VkBufferUsageFlags usa
 
     if (vmaCreateBuffer(allocator, &bufferCreateInfo, &allocationCreateInfo, &mBuffer, &mAllocation, &allocationInfo) != VK_SUCCESS) 
     {
-        throw std::runtime_error("Could not create a VMA Buffer, probably ran out of graphics memory.");
+        throw std::runtime_error("Could not create a VMA Buffer, might've ran out of graphics memory.");
     }
 }
 
 Buffer::~Buffer()
 {
     vmaDestroyBuffer(mAllocator, mBuffer, mAllocation);
+}
+
+BufferView::BufferView(VkDevice device, VkBuffer buffer, VkFormat format, VkDeviceSize offset, VkDeviceSize range)
+    : mDevice(device)
+{
+    VkBufferViewCreateInfo bufferViewCreateInfo = {};
+    bufferViewCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_VIEW_CREATE_INFO;
+    bufferViewCreateInfo.pNext = nullptr;
+    bufferViewCreateInfo.flags = 0;
+    bufferViewCreateInfo.buffer = buffer;
+    bufferViewCreateInfo.format = format;
+    bufferViewCreateInfo.offset = offset;
+    bufferViewCreateInfo.range = range;
+
+    if (vkCreateBufferView(mDevice, &bufferViewCreateInfo, nullptr, &mBufferView) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Could not create a buffer view!");
+    }
+}
+
+BufferView::~BufferView()
+{
+    vkDestroyBufferView(mDevice, mBufferView, nullptr);
+}
+
+Image::Image(VmaAllocator allocator, uint32_t graphicsQueueIndex, VkImageType imageType, VkFormat format, VkExtent3D extent, uint32_t mipLevels, VkImageTiling tiling, VkImageUsageFlags usage, VmaMemoryUsage memoryUsage, VkMemoryPropertyFlags requiredFlags)
+    : mAllocator(allocator)
+{
+
+    VkImageCreateInfo imageCreateInfo = {};
+    imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    imageCreateInfo.pNext = nullptr;
+    imageCreateInfo.flags = 0;
+    imageCreateInfo.imageType = imageType;
+    imageCreateInfo.format = format;
+    imageCreateInfo.extent = extent;
+    imageCreateInfo.mipLevels = mipLevels;
+    imageCreateInfo.arrayLayers = 1;
+    imageCreateInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+    imageCreateInfo.tiling = tiling;
+    imageCreateInfo.usage = usage;
+    imageCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    imageCreateInfo.queueFamilyIndexCount = 1;
+    imageCreateInfo.pQueueFamilyIndices = &graphicsQueueIndex;
+    imageCreateInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+
+    VmaAllocationCreateInfo allocationCreateInfo = {};
+    allocationCreateInfo.flags = 0;
+    allocationCreateInfo.usage = VMA_MEMORY_USAGE_GPU_ONLY;
+    allocationCreateInfo.requiredFlags = memoryUsage;
+    allocationCreateInfo.preferredFlags = 0;
+    allocationCreateInfo.memoryTypeBits = 0;
+    allocationCreateInfo.pool = VK_NULL_HANDLE;
+    allocationCreateInfo.pUserData = nullptr;
+    allocationCreateInfo.priority = 0.0f;
+
+    VmaAllocationInfo allocationInfo = {};
+
+    if (vmaCreateImage(allocator, &imageCreateInfo, &allocationCreateInfo, &mImage, &mAllocation, &allocationInfo) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Could not create a VMA Image!");
+    }
+}
+
+Image::~Image()
+{
+    vmaDestroyImage(mAllocator, mImage, mAllocation);
+}
+
+ImageView::ImageView(VkDevice device, VkImage image, VkImageViewType viewType, VkExtent3D extent, VkFormat format, VkComponentMapping components, VkImageSubresourceRange subresourceRange)
+    : mDevice(device)
+{
+    VkImageViewCreateInfo imageViewCreateInfo = {};
+    imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    imageViewCreateInfo.pNext = nullptr;
+    imageViewCreateInfo.flags = 0;
+    imageViewCreateInfo.image = image;
+    imageViewCreateInfo.viewType = viewType;
+    imageViewCreateInfo.format = format;
+    imageViewCreateInfo.components = components;
+    imageViewCreateInfo.subresourceRange = subresourceRange;
+
+    if (vkCreateImageView(mDevice, &imageViewCreateInfo, nullptr, &mImageView) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Could not create an image view!");
+    }
 }
--- Source/RenderDevice.h
+++ Source/RenderDevice.h
@@ -1,41 +1,108 @@
 #pragma once
 
+#include <_types/_uint32_t.h>
 #include <vulkan/vulkan.h>
 #include "vk_mem_alloc.h"
+#include "vulkan/vulkan_core.h"
 
 class RenderDevice;
 class Buffer;
+class Image;
 
 class Buffer 
 {
+
 public: 
     Buffer(VmaAllocator allocator, VkDeviceSize size, VkBufferUsageFlags usage, uint32_t graphicsQueueFamilyIndex, VmaMemoryUsage memoryUsage, VkMemoryPropertyFlags requiredFlags);
     ~Buffer();
+
+    VkBuffer getBuffer() {return mBuffer;}
 
 private:
     VmaAllocator    mAllocator;
     VkBuffer        mBuffer;
     VmaAllocation   mAllocation;
+
+};
+
+class BufferView
+{
+
+public:
+    BufferView(VkDevice device, VkBuffer buffer, VkFormat format, VkDeviceSize offset, VkDeviceSize range);
+    ~BufferView();
+
+    VkBufferView getBufferView() {return mBufferView;}
+
+private:
+    VkDevice mDevice;
+    VkBufferView mBufferView;
+};
+
+class Image
+{
+public:
+    Image(VmaAllocator allocator, uint32_t graphicsQueueIndex, VkImageType imageType, VkFormat format, VkExtent3D extent, uint32_t mipLevels, VkImageTiling tiling, VkImageUsageFlags usage, VmaMemoryUsage memoryUsage, VkMemoryPropertyFlags requiredFlags);
+    ~Image();
+
+public:
+    VkImage getImage() {return mImage;}
+
+private:
+    VmaAllocator  mAllocator;
+    VkImage       mImage;
+    VmaAllocation mAllocation;
+
+};
+
+class ImageView
+{
+    
+public:
+    ImageView(VkDevice device, VkImage image, VkImageViewType viewType, VkExtent3D extent, VkFormat format, VkComponentMapping components, VkImageSubresourceRange subresourceRange);
+    ~ImageView();
+
+    VkImageView getImageView() {return mImageView;}
+
+private:
+    VkDevice mDevice;
+    VkImageView mImageView;
+
 };
 
 class RenderDevice 
 {
 
 public:
-    RenderDevice();
+    RenderDevice(uint32_t physicalDeviceIndex = 0);
     ~RenderDevice();
 
+    VkInstance       getInstance() {return mInstance;}
+    uint32_t         getPhysicalDeviceIndex() {return mPhysicalDeviceIndex;}
+    VkPhysicalDevice getPhysicalDevice() {return mPhysicalDevice;}
+    VkDevice         getDevice() {return mDevice;}
+    uint32_t         getGraphicsQueueIndex() {return mGraphicsQueueIndex;}
+    uint32_t         getPresentQueueIndex() {return mPresentQueueIndex;}
+    bool             isGraphicsAndPresentQueueIndexSame() {return mGraphicsQueueIndex == mPresentQueueIndex;}
+    VkQueue          getGraphicsQueue() {return mGraphicsQueue;}
+    VkQueue          getPresentQueue() {return mPresentQueue;}
 
-    Buffer* CreateBuffer(VkDeviceSize size, VkBufferUsageFlagBits usage, VmaMemoryUsage memoryUsage);
-
-
+    Buffer*          createBuffer(VkDeviceSize size, VkBufferUsageFlagBits usage, VmaMemoryUsage memoryUsage);
+    BufferView*      createBufferView(VkBuffer buffer, VkFormat format, VkDeviceSize offset, VkDeviceSize range);
+    Image*           createImage(VkImageType imageType, VkFormat format, VkExtent3D extent, uint32_t mipLevels, VkImageTiling tiling, VkImageUsageFlags usage, VmaMemoryUsage memoryUsage, VkMemoryPropertyFlags requiredFlags);
+    ImageView*       createImageView(VkImage image, VkImageViewType viewType, VkExtent3D extent, VkFormat format, VkComponentMapping components, VkImageSubresourceRange subresourceRange);
+    
 private:
-    VkInstance mInstance;
+    VkInstance               mInstance;
 #ifndef NDEBUG
     VkDebugUtilsMessengerEXT mDebugMessenger;
 #endif
-    VkPhysicalDevice mPhysicalDevice;
-    VkDevice mDevice;
-    VmaAllocator mAllocator;
+    uint32_t                 mPhysicalDeviceIndex;
+    VkPhysicalDevice         mPhysicalDevice;
+    uint32_t                 mGraphicsQueueIndex, mPresentQueueIndex; 
+    VkDevice                 mDevice;
+    VkQueue                  mGraphicsQueue, mPresentQueue;
+    VkRenderPass             mRenderPass;
+    VmaAllocator             mAllocator;
     
 };
