2f4181946213a78747084942125fabfca41ef856
cullvox <cullvox@outlook.com>
2023-01-10 12:01:33 -0500

    Adding the more recent architecture of the project.

--- Source/Vulkan/Platform/VulkanPlatformLinux.h
+++ Source/Vulkan/Platform/VulkanPlatformLinux.h
@@ -2,7 +2,6 @@
 
 class VulkanPlatformLinux
 {
-
 public:
     static const char* getSurfaceExtensionName() {return "VK_KHR_xcb_surface";}
     static VkSurfaceKHR createSurface(VkInstance instance);
